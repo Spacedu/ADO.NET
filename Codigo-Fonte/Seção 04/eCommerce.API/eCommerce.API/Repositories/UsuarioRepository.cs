08c3504e508531a9f7654f5aac091ac3e5c8d51c
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2021-08-26 20:16:24 -0300

    S06E02
    - Adicionado Endereços de Entrega.

--- Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -74,36 +74,62 @@ namespace eCommerce.API.Repositories
             {
                 
                 SqlCommand command = new SqlCommand();
-                command.CommandText = $"SELECT * FROM Usuarios u LEFT JOIN Contatos c ON c.UsuarioId = u.Id WHERE u.Id = @Id";
+                command.CommandText = $"SELECT * FROM Usuarios u LEFT JOIN Contatos c ON c.UsuarioId = u.Id LEFT JOIN EnderecosEntrega ee ON ee.UsuarioId = u.Id WHERE u.Id =  @Id";
                 command.Parameters.AddWithValue("@Id", id);
                 command.Connection = (SqlConnection)_connection;
 
                 _connection.Open();
                 SqlDataReader dataReader = command.ExecuteReader();
 
+                Dictionary<int, Usuario> usuarios = new Dictionary<int, Usuario>();
+
                 while (dataReader.Read())
                 {
                     Usuario usuario = new Usuario();
-                    usuario.Id = dataReader.GetInt32(0);
-                    usuario.Nome = dataReader.GetString("Nome");
-                    usuario.Email = dataReader.GetString("Email");
-                    usuario.Sexo = dataReader.GetString("Sexo");
-                    usuario.RG = dataReader.GetString("RG");
-                    usuario.CPF = dataReader.GetString("CPF");
-                    usuario.NomeMae = dataReader.GetString("NomeMae");
-                    usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
-                    usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+                    if (!(usuarios.ContainsKey(dataReader.GetInt32(0))))
+                    {                        
+                        usuario.Id = dataReader.GetInt32(0);
+                        usuario.Nome = dataReader.GetString("Nome");
+                        usuario.Email = dataReader.GetString("Email");
+                        usuario.Sexo = dataReader.GetString("Sexo");
+                        usuario.RG = dataReader.GetString("RG");
+                        usuario.CPF = dataReader.GetString("CPF");
+                        usuario.NomeMae = dataReader.GetString("NomeMae");
+                        usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                        usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
 
-                    Contato contato = new Contato();
-                    contato.Id = dataReader.GetInt32(9);
-                    contato.UsuarioId = usuario.Id;
-                    contato.Telefone = dataReader.GetString("Telefone");
-                    contato.Celular = dataReader.GetString("Celular");
+                        Contato contato = new Contato();
+                        contato.Id = dataReader.GetInt32(9);
+                        contato.UsuarioId = usuario.Id;
+                        contato.Telefone = dataReader.GetString("Telefone");
+                        contato.Celular = dataReader.GetString("Celular");
 
-                    usuario.Contato = contato;
+                        usuario.Contato = contato;
 
-                    return usuario;
+                        usuarios.Add(usuario.Id, usuario);
+                    }
+                    else
+                    {
+                        usuario = usuarios[dataReader.GetInt32(0)];
+                    }                    
+
+                    EnderecoEntrega enderecoEntrega = new EnderecoEntrega();
+                    enderecoEntrega.Id = dataReader.GetInt32(13);
+                    enderecoEntrega.UsuarioId = usuario.Id;
+                    enderecoEntrega.NomeEndereco = dataReader.GetString("NomeEndereco");
+                    enderecoEntrega.CEP = dataReader.GetString("CEP");
+                    enderecoEntrega.Estado = dataReader.GetString("Estado");
+                    enderecoEntrega.Cidade = dataReader.GetString("Cidade");
+                    enderecoEntrega.Bairro = dataReader.GetString("Bairro");
+                    enderecoEntrega.Endereco = dataReader.GetString("Endereco");
+                    enderecoEntrega.Numero = dataReader.GetString("Numero");
+                    enderecoEntrega.Complemento = dataReader.GetString("Complemento");
+
+                    usuario.EnderecosEntrega = (usuario.EnderecosEntrega == null) ? new List<EnderecoEntrega>() : usuario.EnderecosEntrega;
+                    usuario.EnderecosEntrega.Add(enderecoEntrega);
                 }
+                
+                return usuarios[usuarios.Keys.First()];
             }
             finally
             {
