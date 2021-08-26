c5d6e7d473c5b2a6d7cf0fbd4762159e4f9fbc2f
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2021-08-26 11:22:03 -0300

    S05E04
    - Pegar usuarios do banco.

--- Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -6,6 +6,13 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
+/*
+ * Connection => Estabelecer conexão com banco.
+ * Command => INSERT, UPDATE, DELETE.
+ * DataReader => Arq. Conectada. SELECT.
+ * DataAdapter => Arq. Desconectada. SELECT.
+ */
+
 namespace eCommerce.API.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
@@ -17,22 +24,42 @@ namespace eCommerce.API.Repositories
         {
             _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
-
-
-
-
-
-
-
-        private static List<Usuario> _db = new List<Usuario>()
-        {
-            new Usuario(){ Id=1, Nome="Filipe Rodrigues", Email="filipe.rodrigues@gmail.com" },
-            new Usuario(){ Id=2, Nome="Marcelo Rodrigues", Email="marcelo.rodrigues@gmail.com"},
-            new Usuario(){ Id=3, Nome="Jessica Rodrigues", Email="jessica.rodrigues@gmail.com"}
-        };
+        
         public List<Usuario> Get()
         {
-            return _db;
+            List<Usuario> usuarios = new List<Usuario>();
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT * FROM Usuarios";
+                command.Connection = (SqlConnection)_connection;
+
+                _connection.Open();
+
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Usuario usuario = new Usuario();
+                    usuario.Id = dataReader.GetInt32("Id");
+                    usuario.Nome = dataReader.GetString("Nome");
+                    usuario.Email = dataReader.GetString("Email");
+                    usuario.Sexo = dataReader.GetString("Sexo");
+                    usuario.RG = dataReader.GetString("RG");
+                    usuario.CPF = dataReader.GetString("CPF");
+                    usuario.NomeMae = dataReader.GetString("NomeMae");
+                    usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                    usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                    usuarios.Add(usuario);
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return usuarios;
         }
 
         public Usuario Get(int id)
@@ -55,5 +82,12 @@ namespace eCommerce.API.Repositories
         {
             _db.Remove(_db.FirstOrDefault(a => a.Id == id));
         }
+
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario(){ Id=1, Nome="Filipe Rodrigues", Email="filipe.rodrigues@gmail.com" },
+            new Usuario(){ Id=2, Nome="Marcelo Rodrigues", Email="marcelo.rodrigues@gmail.com"},
+            new Usuario(){ Id=3, Nome="Jessica Rodrigues", Email="jessica.rodrigues@gmail.com"}
+        };
     }
 }
