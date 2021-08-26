9486426dc3c4bbc7b1f37fa544883bfcd1c2f5cd
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2021-08-26 13:07:03 -0300

    S05E08
    - Adicionado Inserir usuário.

--- Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -107,7 +107,27 @@ namespace eCommerce.API.Repositories
 
         public void Insert(Usuario usuario)
         {
-            _db.Add(usuario);
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "INSERT INTO Usuarios(Nome, Email, Sexo, RG, CPF, NomeMae, SituacaoCadastro, DataCadastro) VALUES (@Nome, @Email, @Sexo, @RG, @CPF, @NomeMae, @SituacaoCadastro, @DataCadastro)";
+                command.Connection = (SqlConnection)_connection;
+
+                command.Parameters.AddWithValue("@Nome", usuario.Nome);
+                command.Parameters.AddWithValue("@Email", usuario.Email);
+                command.Parameters.AddWithValue("@Sexo", usuario.Sexo);
+                command.Parameters.AddWithValue("@RG", usuario.RG);
+                command.Parameters.AddWithValue("@CPF", usuario.CPF);
+                command.Parameters.AddWithValue("@NomeMae", usuario.NomeMae);
+                command.Parameters.AddWithValue("@SituacaoCadastro", usuario.SituacaoCadastro);
+                command.Parameters.AddWithValue("@DataCadastro", usuario.DataCadastro);
+
+                _connection.Open();
+                command.ExecuteNonQuery();
+            }finally
+            {
+                _connection.Close();
+            }
         }
 
         public void Update(Usuario usuario)
