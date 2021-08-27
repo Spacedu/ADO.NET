5b195b143eb22a83a75fb606bc4309cebdac8ac2
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2021-08-27 11:08:25 -0300

    S06E05
    - Inserir usuário com Endereço de Entrega

--- Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -185,6 +185,26 @@ namespace eCommerce.API.Repositories
 
                 usuario.Contato.UsuarioId = usuario.Id;
                 usuario.Contato.Id = (int)command.ExecuteScalar();
+
+                foreach(var endereco in usuario.EnderecosEntrega)
+                {
+                    command = new SqlCommand();
+                    command.Connection = (SqlConnection)_connection;
+
+                    command.CommandText = "INSERT INTO EnderecosEntrega (UsuarioId, NomeEndereco, CEP, Estado, Cidade, Bairro, Endereco, Numero, Complemento) VALUES (@UsuarioId, @NomeEndereco, @CEP, @Estado, @Cidade, @Bairro, @Endereco, @Numero, @Complemento); SELECT CAST(scope_identity() AS int)";
+                    command.Parameters.AddWithValue("@UsuarioId", usuario.Id);
+                    command.Parameters.AddWithValue("@NomeEndereco", endereco.NomeEndereco);
+                    command.Parameters.AddWithValue("@CEP", endereco.CEP);
+                    command.Parameters.AddWithValue("@Estado", endereco.Estado);
+                    command.Parameters.AddWithValue("@Cidade", endereco.Cidade);
+                    command.Parameters.AddWithValue("@Bairro", endereco.Bairro);
+                    command.Parameters.AddWithValue("@Endereco", endereco.Endereco);
+                    command.Parameters.AddWithValue("@Numero", endereco.Numero);
+                    command.Parameters.AddWithValue("@Complemento", endereco.Complemento);
+
+                    endereco.Id = (int)command.ExecuteScalar();
+                    endereco.UsuarioId = usuario.Id;
+                }
             }
             finally
             {
