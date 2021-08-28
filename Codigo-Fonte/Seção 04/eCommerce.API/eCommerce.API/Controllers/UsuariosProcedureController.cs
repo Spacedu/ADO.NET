23e5c01850c2b9e0ba74364db1948b46c93bd8b3
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2021-08-27 22:56:32 -0300

    S0701
    - Estruturando Controlador e Repository UsuarioProcedure

---
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Controllers/UsuariosProcedureController.cs
@@ -0,0 +1,75 @@
+﻿using eCommerce.API.Models;
+using eCommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosProcedureController : ControllerBase
+    {
+        private IUsuarioRepository _repository;
+        public UsuariosProcedureController()
+        {
+            _repository = new UsuarioProcedureRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+
+            if (usuario == null)
+            {
+                return NotFound(); //ERRO HTTP: 404 - Not Found
+            }
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody] Usuario usuario)
+        {
+            try
+            {
+                _repository.Insert(usuario);
+                return Ok(usuario);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] Usuario usuario)
+        {
+            try
+            {
+                _repository.Update(usuario);
+                return Ok(usuario);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioProcedureRepository.cs
@@ -0,0 +1,42 @@
+﻿using eCommerce.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuariosProcedureRepository : IUsuarioRepository
+    {
+        private IDbConnection _connection;
+        public UsuariosProcedureRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        public List<Usuario> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
